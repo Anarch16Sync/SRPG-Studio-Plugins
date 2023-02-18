ad78ec1541bb53c76cb0ca1e1e283d3de948f403
Anarch16Sync <Anarch16Sync@gmail.com>
2023-02-18 12:02:07 -0300

    Now it works properly

--- A16S_Plugins/Class Plugins/Class Fixed Growth.js
+++ A16S_Plugins/Class Plugins/Class Fixed Growth.js
@@ -1,4 +1,7 @@
-/* This plugin adds the Fixed Class growths as fixed stat increases on a player unit level up. 
+/* This plugin adds the Fixed Class growths that is set on the class prototype info as fixed stat increases on a player unit level up. 
+
+How to use: Add into your plugin folder, set the desired Growth Settings under prototype info on the Class tab. 
+Now player characters will gain the same stats as a enemy unit when level up plus their own personal growths.
 
 Author: Anarch16Sync
 */
@@ -8,13 +11,14 @@ ExperienceControl._createGrowthArray = function(unit) {
     var count = ParamGroup.getParameterCount();
     var growthArray = [];
     var weapon = ItemControl.getEquippedWeapon(unit);
+    var classGrowth = unit.getClass().getPrototypeInfo().getGrowthArray(unit.getLv());
     
     for (i = 0; i < count; i++) {
         // Calculate the growth value (or the growth rate).
         n = ParamGroup.getGrowthBonus(unit, i) + ParamGroup.getUnitTotalGrowthBonus(unit, i, weapon);
         
         // Set the rise value.
-        growthArray[i] = this._getGrowthValue(n) + unit.getClass().getPrototypeInfo().getGrowthArray(unit.getLv());
+        growthArray[i] = this._getGrowthValue(n) + classGrowth[i];
     }
     
     return growthArray;
