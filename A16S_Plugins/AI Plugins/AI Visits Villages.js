5b689346d403b2ee15578b99424f53649b560084
Anarch16Sync <Anarch16Sync@gmail.com>
2024-03-03 21:29:34 -0300

    Update AI Visits Villages.js
    Updated conditions for compatibility with AI Sieges

--- A16S_Plugins/AI Plugins/AI Visits Villages.js
+++ A16S_Plugins/AI Plugins/AI Visits Villages.js
@@ -26,71 +26,67 @@ Created
 Terms of Use: 
 This plugin is provided as is, use at your own discretion. Redistribution and modification is OK.
 */
-
-//Function alias
-var aliasCombinationCollector = CombinationCollector.Skill._setCombination;
-
-//Adds the check for the RAIDER skill to create a proper combination
-CombinationCollector.Skill._setCombination = function(misc) {
-		var skillType = misc.skill.getSkillType();
-		if (skillType === SkillType.CUSTOM) {
-			if (misc.skill.getCustomKeyword() === 'RAIDER') {
-			this._setRaiderCombination(misc)	
+(function() {
+	//Function alias
+	var aliasCombinationCollector = CombinationCollector.Skill._setCombination;
+	
+	//Adds the check for the RAIDER skill to create a proper combination
+	CombinationCollector.Skill._setCombination = function(misc) {
+			var skillType = misc.skill.getSkillType();
+			if (skillType === SkillType.CUSTOM && misc.skill.getCustomKeyword() === 'RAIDER') {
+				this._setRaiderCombination(misc)
+			} else {
+				aliasCombinationCollector.call(this,misc)
 			}
-		} else {
-			aliasCombinationCollector.call(this,misc)
-		}
+		};
+	
+	//Combiantion for the Raiders, makes them go to Village Place Events	
+	CombinationCollector.Skill._setRaiderCombination = function(misc){
+		var rangeMetrics;
+		rangeMetrics = StructureBuilder.buildRangeMetrics();
+		rangeMetrics.endRange = 0;
+		rangeMetrics.rangeType = SelectionRangeType.SELFONLY;
+		this._setPlaceRangeCombination(misc, PlaceEventFilterFlag.VILLAGE, rangeMetrics);
 	};
-
-//Combiantion for the Raiders, makes them go to Village Place Events	
-CombinationCollector.Skill._setRaiderCombination = function(misc){
-	var rangeMetrics;
-	rangeMetrics = StructureBuilder.buildRangeMetrics();
-	rangeMetrics.endRange = 0;
-	rangeMetrics.rangeType = SelectionRangeType.SELFONLY;
-	this._setPlaceRangeCombination(misc, PlaceEventFilterFlag.VILLAGE, rangeMetrics);
-};
-
-//Function alias
-var aliasSkillAutoActionMSU = SkillAutoAction._moveSkillUse;
-var aliasSkillAutoActionESU = SkillAutoAction._enterSkillUse;
-
-//I have no idea what this does, but it had checks for other skills so now it has for RAIDER
-SkillAutoAction._moveSkillUse = function() {
-		var result = MoveResult.CONTINUE;
-		var skillType = this._skill.getSkillType();
-		
-		if (skillType === SkillType.CUSTOM) {
-			if (this._skill.getCustomKeyword() === 'RAIDER') {
-			result = this._eventTrophy.moveEventTrophyCycle();	
+	
+	//Function alias
+	var aliasSkillAutoActionMSU = SkillAutoAction._moveSkillUse;
+	var aliasSkillAutoActionESU = SkillAutoAction._enterSkillUse;
+	
+	//I have no idea what this does, but it had checks for other skills so now it has for RAIDER
+	SkillAutoAction._moveSkillUse = function() {
+			var result = MoveResult.CONTINUE;
+			var skillType = this._skill.getSkillType();
+			
+			if (skillType === SkillType.CUSTOM && this._skill.getCustomKeyword() === 'RAIDER') {
+				result = this._eventTrophy.moveEventTrophyCycle();	
+			} else {
+				return aliasSkillAutoActionMSU.call(this)
 			}
-		} else {
-			return aliasSkillAutoActionMSU.call(this)
-		}
-		return result
-	};
-
-//This is the part that makes the AI choose what skill to use, so the check for RAIDER needs to be added
-SkillAutoAction._enterSkillUse = function() {
-		var result = EnterResult.NOTENTER;
-		var skillType = this._skill.getSkillType();
-		
-		if (skillType === SkillType.CUSTOM) {
-			if (this._skill.getCustomKeyword() === 'RAIDER') {
-			result = this._enterRaiding();	
+			return result
+		};
+	
+	//This is the part that makes the AI choose what skill to use, so the check for RAIDER needs to be added
+	SkillAutoAction._enterSkillUse = function() {
+			var result = EnterResult.NOTENTER;
+			var skillType = this._skill.getSkillType();
+			
+			if (skillType === SkillType.CUSTOM && this._skill.getCustomKeyword() === 'RAIDER') {
+				result = this._enterRaiding();	
+			} else {
+				return aliasSkillAutoActionESU.call(this)
 			}
-		} else {
-			return aliasSkillAutoActionESU.call(this)
-		}
-		return result
-		
-	};
-
-//and this is the actual triggering of the event... well, enterEventTrohpyCycle does the heavy lifting, but I called it so I deserve some credit to.
-SkillAutoAction._enterRaiding = function() {
-		var event = PosChecker.getPlaceEventFromPos(PlaceEventType.VILLAGE,this._targetPos.x, this._targetPos.y);
-		
-		this._eventTrophy = createObject(EventTrophy);
-		
-		return this._eventTrophy.enterEventTrophyCycle(this._unit, event);
-	};
+			return result
+			
+		};
+	
+	//and this is the actual triggering of the event... well, enterEventTrohpyCycle does the heavy lifting, but I called it so I deserve some credit to.
+	SkillAutoAction._enterRaiding = function() {
+			var event = PosChecker.getPlaceEventFromPos(PlaceEventType.VILLAGE,this._targetPos.x, this._targetPos.y);
+			
+			this._eventTrophy = createObject(EventTrophy);
+			
+			return this._eventTrophy.enterEventTrophyCycle(this._unit, event);
+		};
+	
+	})();
