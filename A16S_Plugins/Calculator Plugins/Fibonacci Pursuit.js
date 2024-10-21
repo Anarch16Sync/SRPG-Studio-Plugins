3943540bdb1b65e7fb61aaa86333deaad25ab8ea
Anarch16Sync <Anarch16Sync@gmail.com>
2024-10-21 18:17:28 -0300

    Update Fibonacci Pursuit.js

--- A16S_Plugins/Calculator Plugins/Fibonacci Pursuit.js
+++ A16S_Plugins/Calculator Plugins/Fibonacci Pursuit.js
@@ -4,7 +4,7 @@ Fibonacci Pursuit Plugin
 This plugin makes it so that is possible to have more than 2 rounds of combat based on how much big is the Agility difference,
 but each extra round of combat needs an agility increase that follows the Fibonacci sequence.
 
-i.e with a pursuit value of 3 in the settings, the thresholds for rounds are like this:
+i.e with a pursuit diffReq of 3 in the settings, the thresholds for rounds are like this:
 Agility Diff    Rounds
     3           2       
     8           3
@@ -13,10 +13,10 @@ Agility Diff    Rounds
 
 and so on and so on.
 
-The pursuit value in the setting now determines in what value of the sequence the threshold starts
+The pursuit diffReq in the setting now determines in what diffReq of the sequence the threshold starts
 
-ie a pursuit value of 3, means that the agility difference starts at 3, but a persuit value of 5 means the difference needed starts at 8,
-because 8 is the 5th value in the sequence.
+ie a pursuit diffReq of 3, means that the agility difference starts at 3, but a persuit diffReq of 5 means the difference needed starts at 8,
+because 8 is the 5th diffReq in the sequence.
 
 Optional Settings:
 This can be changed to required a special skill that allows to trigger the extra rounds, this allows it to coexist with the default pursuit
@@ -40,31 +40,35 @@ var SKILL_FIBONACCI_PURSUIT = 'fibosuit16'; 	// skill keyword
 //------------------------------------------
 //Code Stuff below
 //------------------------------------------
-Calculator.getDifference = function(index) {
-    var fibo=[0,1,2,3,5,8,13,21];
-    if (index == null) {
-        index = DataConfig.getRoundDifference();
-    }
-
-    var a,b,c;
-
-    if (index >= fibo.length) {
-        var a = fibo[fibo.length-2];
-        var b = fibo[fibo.length-1];
-
-        for (var i = fibo.length; i <= index; i++) {
-        c = a + b;
-        a = b;
-        b = c;
-        }
-
-        return b;
-    }
-    return fibo[index];
-};
-
 (function(){
     var alias001 = Calculator.calculateRoundCount;
+    var alias002 = Calculator.getDifference;
+
+    Calculator.getDifference = function(index) {
+        var fibo=[0,1,2,3,5,8,13,21];
+        if (index == null) {
+            if(REQUIRES_SKILL){
+                return alias002.call(this)
+            }
+            index = DataConfig.getRoundDifference();
+        }
+    
+        var a,b,c;
+    
+        if (index >= fibo.length) {
+            var a = fibo[fibo.length-2];
+            var b = fibo[fibo.length-1];
+    
+            for (var i = fibo.length; i <= index; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+            }
+    
+            return b;
+        }
+        return fibo[index];
+    };
     
     Calculator.calculateRoundCount = function(active, passive, weapon, activeTotalStatus,passiveTotalStatus) {
         if(typeof SupportSkillControl !== 'undefined'){
@@ -75,7 +79,7 @@ Calculator.getDifference = function(index) {
 
         var skill = SkillControl.getPossessionCustomSkill(active, SKILL_FIBONACCI_PURSUIT);
 		if( skill !== null || !REQUIRES_SKILL ){
-            var value, index,b,c;
+            var diffReq, index, nDiff,c;
 
             if(typeof SupportSkillControl !== 'undefined'){
                 activeAgi = AbilityCalculator.getAgility(active, weapon) + this.getAgilityPlus(active, passive, weapon, activeTotalStatus);
@@ -86,18 +90,18 @@ Calculator.getDifference = function(index) {
             }
 
             index = DataConfig.getRoundDifference()+(rounds-1);
-            value = this.getDifference(index);
-            b=this.getDifference(index+1)
-            var diff = (activeAgi-passiveAgi)-this.getDifference(index-1);
+            diffReq = this.getDifference(index);
+            nDiff=this.getDifference(index+1)
+            var agiAdv = (activeAgi-passiveAgi)-this.getDifference(index-1);
             
-            while (diff >= value){
-                    diff-=value;
+            while (agiAdv >= diffReq){
+                agiAdv-=diffReq;
 
-                    c=b;
-                    b+=value;
+                c=nDiff;
+                nDiff+=diffReq;
                     
-                    value = c;
-                    rounds++;
+                diffReq = c;
+                rounds++;
         
             }
         }
